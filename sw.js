228a444a0d3e4e78afaf65300f7694159173f31b
emac2023 <eric.mackinnon10@gmail.com>
2026-08-18 14:04:25 -0600

    Add North Sky Watch service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='north-sky-watch-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const r=e.request;if(r.method!=='GET')return;const u=new URL(r.url);if(u.hostname==='api.weather.gov'||u.hostname==='api.weather.gc.ca'){e.respondWith(fetch(r));return;}if(u.origin===self.location.origin){e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(r,copy));return res;})));return;}e.respondWith(fetch(r).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(r,copy));return res;}).catch(()=>caches.match(r)));});
